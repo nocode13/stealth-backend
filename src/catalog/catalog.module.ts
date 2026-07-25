6912dd1de0977c76d8a574956a515174d750cdbf
nocode13 <azizovxikmatjon04@gmail.com>
2026-07-25 23:59:55 +0500

    fix

--- src/catalog/catalog.module.ts
+++ src/catalog/catalog.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CategoriesModule } from '../categories/categories.module';
 import { CatalogService } from './catalog.service';
+import { StorageModule } from 'src/storage/storage.module';
 
 @Module({
-  imports: [CategoriesModule],
+  imports: [CategoriesModule, StorageModule],
   providers: [CatalogService],
   exports: [CatalogService],
 })
--- src/sellers/sellers.module.ts
+++ src/sellers/sellers.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SellersService } from './sellers.service';
+import { StorageModule } from 'src/storage/storage.module';
 
 @Module({
   providers: [SellersService],
   exports: [SellersService],
+  imports: [StorageModule],
 })
 export class SellersModule {}
